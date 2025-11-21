6ed440e0c019b9d6349a569c80a9ef77e5f17e3d
mmvES <miguerepvfr@gmail.com>
2025-11-21 14:15:41 +0100

    Creacion main

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from app.controller.AppController import AppController
+
+
+app = QApplication(sys.argv)
+app.setApplicationName("Gestor Universidad")
+
+
+controller = AppController()
+# Iniciamos la base de datos cuando este lista (Init_db)
+
+sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()

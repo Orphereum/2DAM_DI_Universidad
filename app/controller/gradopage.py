06240c8a445e43753a7a061b9b11af3e77759a51
AdriTg00 <adrian2000tg@gmail.com>
2026-01-23 14:11:52 +0100

    Remove unnecessary blank line in gradopage.py
    Deleted an extra blank line between import statements to improve code formatting and readability.

--- app/controller/gradopage.py
+++ app/controller/gradopage.py
@@ -1,7 +1,6 @@
 from PySide6.QtWidgets import QWidget, QTableWidgetItem, QMessageBox
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QAbstractItemView
-
 from app.view.GradoPage_ui import Ui_Form
 from app.service.grado_service import GradoService
 from app.models.grado import Grado

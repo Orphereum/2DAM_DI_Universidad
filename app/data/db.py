94a0dee7f860c3e78881fb6ff98363b06bd1f939
AlexPrada05 <babauprada11@gmail.com>
2026-02-16 12:03:19 +0100

    cambio de sitio de base de datos PERO FALTA la ruta implementarla bien

--- app/data/db.py
+++ app/data/db.py
@@ -1,20 +1,20 @@
-import sqlite3
-import os
+    import sqlite3
+    import os
 
 
-# RUTA A LA BASE DE DATOS
+    # RUTA A LA BASE DE DATOS
 
-def _get_db_path():
-    
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_dir, "universidad.db")
+    def _get_db_path():
+        
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_dir, "universidad.db")
 
 
-# CONEXIÓN
+    # CONEXIÓN
 
-def get_connection():
-    db_path = _get_db_path()
-    print("Usando BD:", db_path)
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row 
-    return conn
+    def get_connection():
+        db_path = _get_db_path()
+        print("Usando BD:", db_path)
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row 
+        return conn

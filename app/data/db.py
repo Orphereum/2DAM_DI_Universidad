fc498a0b4b6a886d2340cab5986ae5ae8416beae
AdriTg00 <adrian2000tg@gmail.com>
2026-02-19 12:35:49 +0100

    cambios menores db

--- app/data/db.py
+++ app/data/db.py
@@ -1,26 +1,34 @@
 import sqlite3
 import os
 import sys
+import shutil
 
 def _get_db_path():
-    print("DEBUG: sys.frozen =", getattr(sys, 'frozen', False))
+    # Carpeta persistente del usuario
+    if os.name == 'nt':  # Windows
+        appdata = os.path.join(os.environ['APPDATA'], 'Gestor-Universidad')
+    else:
+        appdata = os.path.expanduser('~/.Gestor-Universidad')
     
-    if getattr(sys, 'frozen', False):  # EXE
-        base_dir = sys._MEIPASS
-        print("DEBUG EXE: base_dir =", base_dir)
-    else:  # DESARROLLO
-        # DESDE app/data/db.py -> subir 3 niveles a RAÍZ proyecto
-        base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        print("DEBUG DEV: base_dir =", base_dir)
+    os.makedirs(appdata, exist_ok=True)
+    user_db = os.path.join(appdata, 'universidad.db')
     
-    db_path = os.path.join(base_dir, "app", "data", "universidad.db")
-    print("DEBUG: db_path =", db_path)
-    return db_path
+    # Si NO existe BD usuario, copia la inicial empaquetada
+    if not os.path.exists(user_db):
+        print("Primera ejecucion: Copiando BD inicial...")
+        if getattr(sys, 'frozen', False):
+            initial_db = os.path.join(sys._MEIPASS, 'app', 'data', 'universidad.db')
+        else:
+            initial_db = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'app', 'data', 'universidad.db')
+        
+        shutil.copy2(initial_db, user_db)
+        print("BD copiada a:", user_db)
+    
+    return user_db
 
 def get_connection():
     db_path = _get_db_path()
-    if not os.path.exists(db_path):
-        raise FileNotFoundError(f"BD no encontrada: {db_path}")
+    print("Usando BD:", db_path)
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row 
     return conn

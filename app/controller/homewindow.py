9953d815c99df3d2f32bdcb38f87c5bd6aa07f5c
AdriTg00 <adrian2000tg@gmail.com>
2026-02-25 12:19:01 +0100

    Cambio de icono de la aplicacion

--- app/controller/homewindow.py
+++ app/controller/homewindow.py
@@ -1,29 +1,39 @@
-#controlador de  la ventana principal
-
 from PySide6.QtWidgets import QMainWindow, QFrame
+from PySide6.QtGui import QIcon
 from app.view.HomeWindow_ui import Ui_HomeWindow
 import os
+import sys
+
+def resource_path(relative_path):
+    """Para PyInstaller"""
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 class HomeWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_HomeWindow()
         self.ui.setupUi(self)
-
+        
+        # TÍTULO E ICONO (nuevo)
+        self.setWindowTitle("Gestor Universidad")
+        self.setWindowIcon(QIcon(resource_path("universidad.ico")))
+        
         w = self.ui.stackedWidget
         w.setMinimumSize(881, 621)
         w.setMaximumSize(881, 621)
 
-
         self.load_styles()
-
         self.fix_margins()
 
     def load_styles(self):
         base_dir = os.path.dirname(os.path.dirname(__file__))
         style_path = os.path.join(base_dir, "styles", "app_style.qss")
 
-        print("Intentando cargar estilos desde:", style_path)  
+        print("Intentando cargar estilos desde:", style_path) 
 
         try:
             with open(style_path, "r", encoding="utf-8") as f:
@@ -50,7 +60,7 @@ class HomeWindow(QMainWindow):
        menu_layout = self.ui.menuWidget.layout()
        if menu_layout:
             menu_layout.setContentsMargins(0, 0, 0, 0)
-            menu_layout.setSpacing(8)  
+            menu_layout.setSpacing(8) 
 
        self.ui.stackedWidget.setContentsMargins(0, 0, 0, 0)
        for i in range(self.ui.stackedWidget.count()):

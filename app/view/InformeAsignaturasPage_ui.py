fd8c4413fabd782e5ad29fcd8c938897de63f8bc
cristobaldp <cdelgadopenaranda@gmail.com>
2026-04-01 14:06:45 +0200

    Update-Final-Informes
    Añadir , filtros + un boton de cerrar y que no se pueda exportar documentos vacios

--- app/view/InformeAsignaturasPage_ui.py
+++ app/view/InformeAsignaturasPage_ui.py
@@ -15,40 +15,53 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QGroupBox,
-    QHBoxLayout, QHeaderView, QLabel, QPushButton,
-    QSizePolicy, QTableWidget, QTableWidgetItem, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QGridLayout,
+    QGroupBox, QHBoxLayout, QHeaderView, QLabel,
+    QPushButton, QSizePolicy, QTableWidget, QTableWidgetItem,
+    QVBoxLayout, QWidget)
+
 class Ui_InformeAsignaturasPage(object):
     def setupUi(self, InformeAsignaturasPage):
         if not InformeAsignaturasPage.objectName():
             InformeAsignaturasPage.setObjectName(u"InformeAsignaturasPage")
-        self.vboxLayout = QVBoxLayout(InformeAsignaturasPage)
-        self.vboxLayout.setObjectName(u"vboxLayout")
+        InformeAsignaturasPage.resize(900, 600)
+        self.verticalLayout = QVBoxLayout(InformeAsignaturasPage)
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.lblTitulo = QLabel(InformeAsignaturasPage)
         self.lblTitulo.setObjectName(u"lblTitulo")
         self.lblTitulo.setAlignment(Qt.AlignCenter)
 
-        self.vboxLayout.addWidget(self.lblTitulo)
+        self.verticalLayout.addWidget(self.lblTitulo)
 
         self.groupFiltros = QGroupBox(InformeAsignaturasPage)
         self.groupFiltros.setObjectName(u"groupFiltros")
         self.gridLayout = QGridLayout(self.groupFiltros)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(self.groupFiltros)
-        self.label.setObjectName(u"label")
+        self.labelTipoInforme = QLabel(self.groupFiltros)
+        self.labelTipoInforme.setObjectName(u"labelTipoInforme")
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.labelTipoInforme, 0, 0, 1, 1)
+
+        self.cb_tipo_informe = QComboBox(self.groupFiltros)
+        self.cb_tipo_informe.addItem("")
+        self.cb_tipo_informe.setObjectName(u"cb_tipo_informe")
+
+        self.gridLayout.addWidget(self.cb_tipo_informe, 0, 1, 1, 1)
+
+        self.labelGrado = QLabel(self.groupFiltros)
+        self.labelGrado.setObjectName(u"labelGrado")
+
+        self.gridLayout.addWidget(self.labelGrado, 1, 0, 1, 1)
 
         self.cb_grado = QComboBox(self.groupFiltros)
         self.cb_grado.setObjectName(u"cb_grado")
 
-        self.gridLayout.addWidget(self.cb_grado, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.cb_grado, 1, 1, 1, 1)
 
-        self.label1 = QLabel(self.groupFiltros)
-        self.label1.setObjectName(u"label1")
+        self.labelCurso = QLabel(self.groupFiltros)
+        self.labelCurso.setObjectName(u"labelCurso")
 
-        self.gridLayout.addWidget(self.label1, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.labelCurso, 2, 0, 1, 1)
 
         self.cb_curso = QComboBox(self.groupFiltros)
         self.cb_curso.addItem("")
@@ -58,12 +71,12 @@ class Ui_InformeAsignaturasPage(object):
         self.cb_curso.addItem("")
         self.cb_curso.setObjectName(u"cb_curso")
 
-        self.gridLayout.addWidget(self.cb_curso, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.cb_curso, 2, 1, 1, 1)
 
-        self.label2 = QLabel(self.groupFiltros)
-        self.label2.setObjectName(u"label2")
+        self.labelCuatrimestre = QLabel(self.groupFiltros)
+        self.labelCuatrimestre.setObjectName(u"labelCuatrimestre")
 
-        self.gridLayout.addWidget(self.label2, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.labelCuatrimestre, 3, 0, 1, 1)
 
         self.cb_cuatrimestre = QComboBox(self.groupFiltros)
         self.cb_cuatrimestre.addItem("")
@@ -71,45 +84,61 @@ class Ui_InformeAsignaturasPage(object):
         self.cb_cuatrimestre.addItem("")
         self.cb_cuatrimestre.setObjectName(u"cb_cuatrimestre")
 
-        self.gridLayout.addWidget(self.cb_cuatrimestre, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.cb_cuatrimestre, 3, 1, 1, 1)
 
-        self.label3 = QLabel(self.groupFiltros)
-        self.label3.setObjectName(u"label3")
+        self.labelTipo = QLabel(self.groupFiltros)
+        self.labelTipo.setObjectName(u"labelTipo")
 
-        self.gridLayout.addWidget(self.label3, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.labelTipo, 4, 0, 1, 1)
 
         self.cb_tipo = QComboBox(self.groupFiltros)
+        self.cb_tipo.addItem("")
+        self.cb_tipo.addItem("")
+        self.cb_tipo.addItem("")
         self.cb_tipo.setObjectName(u"cb_tipo")
 
-        self.gridLayout.addWidget(self.cb_tipo, 3, 1, 1, 1)
+        self.gridLayout.addWidget(self.cb_tipo, 4, 1, 1, 1)
 
 
-        self.vboxLayout.addWidget(self.groupFiltros)
+        self.verticalLayout.addWidget(self.groupFiltros)
 
-        self.tbl_resultados = QTableWidget(InformeAsignaturasPage)
-        self.tbl_resultados.setObjectName(u"tbl_resultados")
-
-        self.vboxLayout.addWidget(self.tbl_resultados)
-
-        self.hboxLayout = QHBoxLayout()
-        self.hboxLayout.setObjectName(u"hboxLayout")
         self.btn_generar = QPushButton(InformeAsignaturasPage)
         self.btn_generar.setObjectName(u"btn_generar")
 
-        self.hboxLayout.addWidget(self.btn_generar)
+        self.verticalLayout.addWidget(self.btn_generar)
 
+        self.tbl_resultados = QTableWidget(InformeAsignaturasPage)
+        if (self.tbl_resultados.columnCount() < 5):
+            self.tbl_resultados.setColumnCount(5)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tbl_resultados.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tbl_resultados.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tbl_resultados.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tbl_resultados.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tbl_resultados.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        self.tbl_resultados.setObjectName(u"tbl_resultados")
+        self.tbl_resultados.setColumnCount(5)
+
+        self.verticalLayout.addWidget(self.tbl_resultados)
+
+        self.horizontalLayoutBotones = QHBoxLayout()
+        self.horizontalLayoutBotones.setObjectName(u"horizontalLayoutBotones")
         self.btn_exportar = QPushButton(InformeAsignaturasPage)
         self.btn_exportar.setObjectName(u"btn_exportar")
 
-        self.hboxLayout.addWidget(self.btn_exportar)
+        self.horizontalLayoutBotones.addWidget(self.btn_exportar)
 
         self.btn_cerrar = QPushButton(InformeAsignaturasPage)
         self.btn_cerrar.setObjectName(u"btn_cerrar")
 
-        self.hboxLayout.addWidget(self.btn_cerrar)
+        self.horizontalLayoutBotones.addWidget(self.btn_cerrar)
 
 
-        self.vboxLayout.addLayout(self.hboxLayout)
+        self.verticalLayout.addLayout(self.horizontalLayoutBotones)
 
 
         self.retranslateUi(InformeAsignaturasPage)
@@ -120,23 +149,41 @@ class Ui_InformeAsignaturasPage(object):
     def retranslateUi(self, InformeAsignaturasPage):
         InformeAsignaturasPage.setWindowTitle(QCoreApplication.translate("InformeAsignaturasPage", u"Informe de Asignaturas", None))
         self.lblTitulo.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Informe de Asignaturas", None))
+        self.lblTitulo.setStyleSheet(QCoreApplication.translate("InformeAsignaturasPage", u"font-size: 20px; font-weight: bold;", None))
         self.groupFiltros.setTitle(QCoreApplication.translate("InformeAsignaturasPage", u"Filtros", None))
-        self.label.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Grado:", None))
-        self.label1.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Curso:", None))
+        self.labelTipoInforme.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Tipo informe:", None))
+        self.cb_tipo_informe.setItemText(0, QCoreApplication.translate("InformeAsignaturasPage", u"Asignaturas", None))
+
+        self.labelGrado.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Grado:", None))
+        self.labelCurso.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Curso:", None))
         self.cb_curso.setItemText(0, QCoreApplication.translate("InformeAsignaturasPage", u"Todos", None))
         self.cb_curso.setItemText(1, QCoreApplication.translate("InformeAsignaturasPage", u"1", None))
         self.cb_curso.setItemText(2, QCoreApplication.translate("InformeAsignaturasPage", u"2", None))
         self.cb_curso.setItemText(3, QCoreApplication.translate("InformeAsignaturasPage", u"3", None))
         self.cb_curso.setItemText(4, QCoreApplication.translate("InformeAsignaturasPage", u"4", None))
 
-        self.label2.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Cuatrimestre:", None))
+        self.labelCuatrimestre.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Cuatrimestre:", None))
         self.cb_cuatrimestre.setItemText(0, QCoreApplication.translate("InformeAsignaturasPage", u"Todos", None))
         self.cb_cuatrimestre.setItemText(1, QCoreApplication.translate("InformeAsignaturasPage", u"1", None))
         self.cb_cuatrimestre.setItemText(2, QCoreApplication.translate("InformeAsignaturasPage", u"2", None))
 
-        self.label3.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Tipo:", None))
+        self.labelTipo.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Tipo:", None))
+        self.cb_tipo.setItemText(0, QCoreApplication.translate("InformeAsignaturasPage", u"Todas", None))
+        self.cb_tipo.setItemText(1, QCoreApplication.translate("InformeAsignaturasPage", u"Obligatorias", None))
+        self.cb_tipo.setItemText(2, QCoreApplication.translate("InformeAsignaturasPage", u"Optativas", None))
+
         self.btn_generar.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Generar informe", None))
-        self.btn_exportar.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Exportar PDF", None))
+        ___qtablewidgetitem = self.tbl_resultados.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Nombre", None));
+        ___qtablewidgetitem1 = self.tbl_resultados.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Cr\u00e9ditos", None));
+        ___qtablewidgetitem2 = self.tbl_resultados.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Curso", None));
+        ___qtablewidgetitem3 = self.tbl_resultados.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Cuatrimestre", None));
+        ___qtablewidgetitem4 = self.tbl_resultados.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Obligatoria", None));
+        self.btn_exportar.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Exportar a PDF", None))
         self.btn_cerrar.setText(QCoreApplication.translate("InformeAsignaturasPage", u"Cerrar", None))
     # retranslateUi
 
